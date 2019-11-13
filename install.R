e42f4e5456c357207166ae7bb7f4b3808c83024b
IOMIDS <56037512+IOMIDS@users.noreply.github.com>
2019-11-13 22:02:13 +0100

    R packages added

--- install.R
+++ install.R
@@ -3,3 +3,15 @@ install.packages("rmarkdown")
 install.packages("httr")
 install.packages("shinydashboard")
 install.packages('leaflet')
+
+# Diabetes
+install.packags('xgboost')
+install.packages('mlbench')
+install.packages('readr')
+
+# Shocksensor
+install.packages('e1071')
+install.packages('randomForest')
+install.packages('caret')
+install.packages('pROC')
+install.packages('partykit')
